31eaec88f916283ba4fa7541a64b9c47292cd646
KKaged <dhigaredaleon707@gmail.com>
2024-01-08 22:07:58 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TikTak from '../public/game-logo.png'
+
+function App() {
+  
+
+  return (
+    <header>
+      <img src={TikTak} alt="React Logo" />
+      <h1>Tic-Tac-Toe</h1>
+    </header>
+  )
+}
+
+export default App
