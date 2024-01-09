0da7cdc7ae1126f411fdddd4e3c6ed748cbf04f4
KKaged <dhigaredaleon707@gmail.com>
2024-01-09 22:13:59 +0900

    prop changes in the player components

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function Player({ name, symbol, nameTag = 'span' }) {
+export default function Player({ name, symbol, }) {
     const [isEditing, setIsEditing] = useState(false)
     const handleEdit = () => {
       setIsEditing(true)
